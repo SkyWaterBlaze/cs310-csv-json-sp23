b6fcd0143bf15fb23467818d7af23ceb798d387b
SkyWaterBlaze <skywbofficial@gmail.com>
2023-02-08 20:12:56 -0600

    Final Project

--- src/edu/jsu/mcis/cs310/Converter.java
+++ src/edu/jsu/mcis/cs310/Converter.java
@@ -1,7 +1,10 @@
 package edu.jsu.mcis.cs310;
 
-import com.github.cliftonlabs.json_simple.*;
+import java.util.*;
 import com.opencsv.*;
+import java.io.StringWriter;
+import java.io.StringReader;
+import com.github.cliftonlabs.json_simple.*;
 
 public class Converter {
     
@@ -29,7 +32,7 @@ public class Converter {
                 "6149-03"
             ],
             "ColHeadings": [
-                "ProdNum",
+                "ProdNum",086w2q
                 "Title",
                 "Season",
                 "Episode",
@@ -77,8 +80,43 @@ public class Converter {
         String result = "{}"; // default return value; replace later!
         
         try {
-        
-            // INSERT YOUR CODE HERE
+      
+            CSVReader csvReader = new CSVReader(new StringReader(csvString));
+            List<String[]> full = csvReader.readAll();
+            Iterator<String[]> iterator = full.iterator();
+       
+            JsonObject jsonRecord = new JsonObject();
+            JsonArray prodNums = new JsonArray();
+            JsonArray data = new JsonArray();
+       
+            if (iterator.hasNext()){
+           
+                String[] headings = iterator.next();
+           
+                while(iterator.hasNext()){
+               
+                    String[] csvRecord = iterator.next();
+                    prodNums.add(csvRecord[0]);
+                    JsonArray internalDataArray = new JsonArray();
+               
+                    for(int i = 1; i < headings.length; ++i){
+                   
+                        if(headings[i].endsWith("Season") || headings[i].endsWith("Episode")) {
+                            internalDataArray.add(Integer.valueOf(csvRecord[i]));
+                        }
+                        else {
+                            internalDataArray.add(csvRecord[i]);
+                        }
+                    }
+                    data.add(internalDataArray);
+                }
+           
+                jsonRecord.put("ProdNums", prodNums);
+                jsonRecord.put("ColHeadings", headings);
+                jsonRecord.put("Data", data);
+            }
+       
+            result = jsonRecord.toJson();
             
         }
         catch (Exception e) {
@@ -96,15 +134,58 @@ public class Converter {
         
         try {
             
-            // INSERT YOUR CODE HERE
+            Map<String, Object> data = new HashMap<>();
+            JsonObject jsonObject = Jsoner.deserialize(jsonString, new JsonObject());
+        
+            JsonArray prodNums = (JsonArray) jsonObject.get("ProdNums");
+            JsonArray colHeadings = (JsonArray) jsonObject.get("ColHeadings");
+            JsonArray dataArray = (JsonArray) jsonObject.get("Data");
+        
+            data.put("ProdNums", prodNums);
+            data.put("ColHeadings", colHeadings);
+            data.put("Data", dataArray);
+        
+            String[] colHeadingsString = new String[colHeadings.size()];
+            for (int i = 0; i < colHeadings.size(); i++) {
+                colHeadingsString[i] = colHeadings.get(i).toString();
+            }
+        
+            StringWriter writer = new StringWriter();
+            CSVWriter csvWriter = new CSVWriter(writer, ',', '"', '\\', "\n");
+            csvWriter.writeNext(colHeadingsString);
+            String csvString = writer.toString();
+        
+            for (int i = 0; i < dataArray.size(); i++) {
+                JsonArray csvRecord = (JsonArray) dataArray.get(i);
+                String prodNumForRecord = prodNums.get(i).toString();
             
+                String[] csvRecordString = new String[csvRecord.size()];
+                for (int j = 0; j < csvRecord.size(); j++) {
+                    String value = csvRecord.get(j).toString();
+                    if (j == 2) {
+                        value = String.format("%02d", Integer.parseInt(value));
+                    }
+                    csvRecordString[j] = value;
+                }
+            
+                String[] combinedArray = new String[csvRecordString.length + 1];
+                combinedArray[0] = prodNumForRecord;
+                System.arraycopy(csvRecordString, 0, combinedArray, 1, csvRecordString.length);
+            
+                csvWriter.writeNext(combinedArray);
+                csvString = writer.toString();
+            }
+        
+            result = csvString;
+        
         }
+        
         catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         return result.trim();
         
     }
-    
+
 }
